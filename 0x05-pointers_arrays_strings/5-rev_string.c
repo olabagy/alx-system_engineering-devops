70fab5c53bbc3fe3378c8cb8db76dec741b27019
aa integrated limited <olabagy@gmail.com>
2022-03-22 19:14:17 +0100

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
+}
